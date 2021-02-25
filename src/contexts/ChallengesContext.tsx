f2c83ac1e970a746dd605e193c2f46ebdac0615b
Lucashmcastro <lucashmcastro@outlook.com>
2021-02-25 15:47:53 -0300

    Created interface Challenge

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,11 +1,18 @@
 import { createContext, useState, ReactNode } from "react";
+import challenges from "../../challenges.json";
 
+interface Challenge {
+  type: "body" | "eye";
+  description: string;
+  amount: number;
+}
 interface ChallengesContextData {
   level: number;
   currentExperience: number;
   challengesCompleted: number;
   levelUp: () => void;
   startNewChallenge: () => void;
+  activeChallenge: Challenge;
 }
 
 interface ChallengesProviderProps {
@@ -19,12 +26,17 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [currentExperience, setCurrentExperience] = useState(0);
   const [challengesCompleted, setChallengesCompleted] = useState(0);
 
+  const [activeChallenge, setActiveChallenge] = useState(null);
+
   function levelUp() {
     setLevel(level + 1);
   }
 
   function startNewChallenge() {
-    console.log("New Challenge");
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+
+    setActiveChallenge(challenge);
   }
 
   return (
@@ -35,6 +47,7 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         challengesCompleted,
         levelUp,
         startNewChallenge,
+        activeChallenge,
       }}
     >
       {children}
